73b3143ae6b76e7812cdb15cb3b4e493b95609ae
Alihtamrawe <ali.h.tamrawe@gmail.com>
2022-08-29 16:06:16 +0300

    last

--- jscript-3-click/jscript.js
+++ jscript-3-click/jscript.js
@@ -22,10 +22,21 @@ var x=0;
 function Like(element)
 {
    x++;
-
+        alert("the ninja was clicked :"+x);
     element.innerText=x+"Likes";
+
+}
+var d=0;
+function Likes(element)
+{
+   d++;
+        alert("the ninja was clicked :"+d);
+    element.innerText=d+"Likes";
 
 }
 function hide(element) {
     element.remove();
+    setTimeout(1000);
+    
+
 }
