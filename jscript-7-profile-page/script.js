7f9316d04e0bda1bf0713a3560d8ac0c22ba2326
Alihtamrawe <ali.h.tamrawe@gmail.com>
2022-08-31 12:51:39 +0300

    almost

--- jscript-7-profile-page/script.js
+++ jscript-7-profile-page/script.js
@@ -1,1 +1,37 @@
 console.log("page loaded...");
+
+
+function change_pic(){
+        var pic=document.querySelector("#pic-change");
+        pic.src="images/anne-s.jpg";
+} 
+
+
+var log = true;
+
+function hide_main(element)
+{ 
+    var main_log = document.querySelector('.main');
+    if(log==true){
+        alert("hiii")
+    element.innerText="sign in";
+    main_log.style.display = "none";
+    log=false;
+}else {
+    element.innertext="sign out";
+    main_log.style.display = "flex";
+    log=true;
+}
+
+
+
+}
+var c =2;
+var c_string;
+function add_conn()
+{
+    var conn = document.querySelector('.badge');
+    c--;
+    c_string+=c;
+    conn.innerText=c_string;
+}
