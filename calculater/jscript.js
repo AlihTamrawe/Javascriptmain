7cc3ef25edbf0299d48640484ff934971a043cf3
Alihtamrawe <ali.h.tamrawe@gmail.com>
2022-09-01 15:31:28 +0300

    finish

---
+++ calculater/jscript.js
@@ -0,0 +1,65 @@
+
+var operator = "";
+var h ="0";
+var num1 = "";
+var num2 = "";
+var flag=0;
+
+function setOP(op)
+{
+    var displayDiv = document.querySelector("#display");
+    operator=op;
+    flag=1;
+    displayDiv.innerText=h;
+}
+function press(x)
+{
+    var displayDiv = document.querySelector("#display");
+    if(flag==0)
+    {
+        num1+=x;
+        console.log(num1);
+        displayDiv.innerText=num1;
+    }
+    else{
+        num2+=x;
+        displayDiv.innerText=num2;
+    }
+}
+
+function calculate(){
+        var result=0.0; 
+         var displayDiv = document.querySelector("#display");
+    if(operator=='/')
+    {
+      
+        result=parseFloat(num1)/parseFloat(num2);
+        displayDiv.innerText =result;
+    }else if(operator=='*'){
+        
+        result=parseFloat(num1)*parseFloat(num2);
+        displayDiv.innerText =result;
+
+    }else if(operator=='-')
+    {
+        
+        result=parseFloat(num1)-parseFloat(num2);
+        displayDiv.innerText =result;
+
+    }else if(operator=='+'){
+        
+        result=parseFloat(num1)+parseFloat(num2);
+        displayDiv.innerText =result;
+
+    }
+
+}
+ function clr()
+ {
+    var displayDiv = document.querySelector("#display");
+    displayDiv.innerText='0';
+    num1="";
+    num2="";
+    flag=0;
+ }
+
